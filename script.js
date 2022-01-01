41953c82672de1368249e16fe9bdfd3f4f3fe095
Kutman7 <kutman.stel@gmail.com>
2022-01-02 01:08:24 +0600

    cleaning

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ x.addListener(myFunction) // Attach listener function on state changes
 
 function myFunction(x) {
     if (x.matches) { // If media query matches
-        document.getElementById("collapsed_nav").style.display = "none";
-        document.getElementById("container_id").style.display = "block";
+        document.getElementById("collapsed-nav").style.display = "none";
+        document.getElementById("container-id").style.display = "block";
         document.getElementById("footer").style.display = "block";
     }
   }
 
 function openMenu() {
-    if (document.getElementById("collapsed_nav").style.display == "" ||
-    document.getElementById("collapsed_nav").style.display == "none") {
-        document.getElementById("collapsed_nav").style.display = "flex";
-        document.getElementById("container_id").style.display = "none";
+    if (document.getElementById("collapsed-nav").style.display == "" ||
+    document.getElementById("collapsed-nav").style.display == "none") {
+        document.getElementById("collapsed-nav").style.display = "flex";
+        document.getElementById("container-id").style.display = "none";
         document.getElementById("footer").style.display = "none";
     }
     else{
-        document.getElementById("collapsed_nav").style.display = "none";
-        document.getElementById("container_id").style.display = "block";
+        document.getElementById("collapsed-nav").style.display = "none";
+        document.getElementById("container-id").style.display = "block";
         document.getElementById("footer").style.display = "block";
     }
     
